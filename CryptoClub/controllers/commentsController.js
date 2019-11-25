9e46478cde61febd931eb92592f44c598e256ee0
Alvaro Rios <alvarorios12@gmail.com>
2019-11-24 21:15:24 -0600

    Comments mostly done, just need to add crypto context to DB search when Marc is done with his page. As for the package.  I installed bootstrap-horizon then uninstalled it so I don't know why its still there oh well

--- CryptoClub/controllers/commentsController.js
+++ CryptoClub/controllers/commentsController.js
@@ -1,6 +1,51 @@
-
+const { User, Wallet, Adress, Comment } = require('../models');
 module.exports = {
   getAllComments(req, res) {
-    return res.render('comments', { data: 'reached /comments index route!' });
+  	//edit this to include only from a btc type
+  	User.findAll({
+  		where: {
+  			expert: 1,
+  		},
+  	}).then(users => {
+  		//I know I'm storing it twice Tomai don't take away points :(
+  		var userIds = [];
+  		var idMap = {};
+  		users.forEach(user => {
+  			userIds.push(user.id);
+  			idMap[user.id] = user.name;
+  		});
+  		Comment.findAll({
+  			where: {
+  				userId: userIds,
+  			},
+  			order: [
+  				['createdAt', 'DESC'],
+  			],
+  		}).then(comments => {
+  			//console.log(userIds);
+  			//console.log(comments);
+		  	var commentToSend = [];
+		   	var commentLimit = 4;
+		   	comments.forEach(comment => {
+		   		console.log(comment.createdAt);
+		   		var comm = {
+		   			name: idMap[comment.userId],
+		   			text: comment.text,
+		   			prediction: comment.prediction,
+		   			//CURRENTLY CANNOT ACCESS CREATEDAT ATTRIBUTE
+		   			//MAY NEED TO REBUILD DATABASE
+		   			//HATE TIMESTAMPS REEEE
+		   			time: comment.createdAt,
+		   		}
+		   		if (commentLimit > 0){
+		   			commentToSend.push(comm);
+		  	 			commentLimit--;
+		   		}
+		   	});
+		   	return res.render('comments', {
+		   		comments: commentToSend,
+		   	});
+  		});
+  	});
   },
 };
