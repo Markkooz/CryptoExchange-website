d40e73942c2c698fbea92911879cbc8ba46c6ea3
Alvaro Rios <alvarorios12@gmail.com>
2019-11-25 20:14:56 -0600

    added  comments for my controllers

--- CryptoClub/controllers/addCommentController.js
+++ CryptoClub/controllers/addCommentController.js
@@ -4,7 +4,10 @@ module.exports = {
     return res.render('addComment');
   },
   postComment(req, res) {
+  	//if errors is empty then user has filled in all fields
   	var errors = [];
+  	//just in case user forgets to add prediction keep the current
+  	//comment for QoL
   	var prefillComment = "";
   	if(!req.body.predict)
   		errors.push("You need a prediction filled in!");
@@ -23,10 +26,14 @@ module.exports = {
   		//do DB processing if comment is valid!
   		//return res.render('addComment');
   		req.session.expertID = 5;
+  		//make a new Comment and push to DB at the same time!
   		Comment.create({
   			text: req.body.comment,
   			prediction: req.body.predict,
   			userId: req.session.expertID,
+  			//(TODO: Alvaro) crypto should not be hardcoded
+  			//should have context from req.body or req.query
+  			//for which type of crypto expert is posting a comment about
   			crypto: "bitcoin",
   		}).then(newComment => {
   			//change to index later
--- CryptoClub/controllers/commentsController.js
+++ CryptoClub/controllers/commentsController.js
@@ -2,12 +2,18 @@ const { User, Wallet, Adress, Comment } = require('../models');
 module.exports = {
   getAllComments(req, res) {
   	//edit this to include only from a btc type
+  	//specifically req.body.crypto (or req.query.crypto) should have
+  	//string of crypto that you want to see comments of
   	User.findAll({
   		where: {
   			expert: 1,
   		},
   	}).then(users => {
   		//I know I'm storing it twice Tomai don't take away points :(
+  		//userIds is just a list of id keys for the DB to pully only comments
+  		//from those users
+ 		// idMap is just a map for instant access for a User's name since Comments
+ 		// only carry the id of the user who made them
   		var userIds = [];
   		var idMap = {};
   		users.forEach(user => {
@@ -19,13 +25,20 @@ module.exports = {
   				userId: userIds,
   			},
   			order: [
+  				//sort fetched list of entries by timestamp in descending order "DESC"
   				['createdAt', 'DESC'],
   			],
   		}).then(comments => {
   			//console.log(userIds);
   			//console.log(comments);
+  			// commentToSend will carry list of Comment objects with the
+  			// following attributes: name, text, prediction, and time (time is not workign atm)
 		  	var commentToSend = [];
+		  	//need this or else all comments get displayed 
 		   	var commentLimit = 4;
+		   	//for each comment in fetched comments, we push to commentToSend
+		   	//(TODO:Alvaro) Make sure only one comment for each user is fetched
+		   	//as-is an expert can spam and take the comment space from other experts
 		   	comments.forEach(comment => {
 		   		console.log(comment.createdAt);
 		   		var comm = {
