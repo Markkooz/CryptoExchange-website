bb5043c8c26372f5a790b3da986d4c51f47fe866
Alvaro Rios <alvarorios12@gmail.com>
2019-11-27 10:17:57 -0600

    accidentally deleted login/reg prompt! Oops. Also added catch for people trying to access user details when they're not logged in

--- CryptoClub/controllers/userDetailsController.js
+++ CryptoClub/controllers/userDetailsController.js
@@ -1,6 +1,8 @@
 
 module.exports = {
   getUser(req, res) {
+  	if (!req.session.user)
+  		return res.redirect('/login');
     return res.render('user', { data: 'reached /user index route!' });
   },
 };
