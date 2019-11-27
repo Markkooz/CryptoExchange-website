5555f2611028a67070b734a6e6bc3d115559aa4f
Alvaro Rios <alvarorios12@gmail.com>
2019-11-26 20:39:51 -0600

    fixed indexcontroller to use the session user yw marc ;)

--- CryptoClub/controllers/indexController.js
+++ CryptoClub/controllers/indexController.js
@@ -3,8 +3,6 @@ const { User, Wallet, Adress, Comment } = require('../models');
 
 module.exports = {
   getIndexData(req, res) {
-    User.findAll().then(db_people=>{
-			return res.render('index',{people:db_people});
-		});
+	return res.render('index',{user: req.session.user});
   },
 };
