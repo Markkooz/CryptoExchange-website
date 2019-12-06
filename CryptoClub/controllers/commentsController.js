2f71e210e1e25d005563f1e657ed57972f4a9d2f
Ricardo Carrillo <ricardo@doesnotexist.utpa.edu>
2019-12-05 22:49:10 -0600

    Began engineering controller for UserList view, and edited the UserList.hbs view to only show users, not comments.

--- CryptoClub/controllers/commentsController.js
+++ CryptoClub/controllers/commentsController.js
@@ -1,8 +1,7 @@
 const { User, Wallet, Adress, Comment } = require('../models');
 module.exports = {
   getAllComments(req, res) {
-    if(!req.query.c || !req.session.user)
-      return res.redirect('/');
+    if(!req.query.c || !req.session.user) return res.redirect('/');
   	//edit this to include only from a btc type
   	//specifically req.body.crypto (or req.query.crypto) should have
   	//string of crypto that you want to see comments of
@@ -25,7 +24,7 @@ module.exports = {
   		Comment.findAll({
   			where: {
   				userId: userIds,
-          crypto: req.query.c,
+          		crypto: req.query.c,
   			},
   			order: [
   				//sort fetched list of entries by timestamp in descending order "DESC"
@@ -39,37 +38,35 @@ module.exports = {
 		  	var commentToSend = [];
 		  	//need this or else all comments get displayed 
 		   	var commentLimit = 4;
-        var foundOne = {};
+        	var foundOne = {};
 		   	//for each comment in fetched comments, we push to commentToSend
 		   	//(TODO:Alvaro) Make sure only one comment for each user is fetched
 		   	//as-is an expert can spam and take the comment space from other experts
 		   	comments.forEach(comment => {
 		   		console.log(comment.createdAt);
-          if (!foundOne[comment.userId]){
-            var comm = {
-              name: idMap[comment.userId],
-              text: comment.text,
-              prediction: comment.prediction,
-              //CURRENTLY CANNOT ACCESS CREATEDAT ATTRIBUTE
-              //MAY NEED TO REBUILD DATABASE
-              //HATE TIMESTAMPS REEEE
-              time: comment.createdAt,
-            }
-            if (commentLimit > 0){
-              commentToSend.push(comm);
-                commentLimit--;
-            }
-            foundOne[comment.userId] = true;
-          }
-          else{
-            //do nothing
-          }
+          		if (!foundOne[comment.userId]){
+					var comm = {
+						name: idMap[comment.userId],
+						text: comment.text,
+						prediction: comment.prediction,
+						//CURRENTLY CANNOT ACCESS CREATEDAT ATTRIBUTE
+						//MAY NEED TO REBUILD DATABASE
+						//HATE TIMESTAMPS REEEE
+						time: comment.createdAt,
+					}
+					if (commentLimit > 0) {
+						commentToSend.push(comm);
+						commentLimit--;
+					}
 
-		   	});
+					foundOne[comment.userId] = true;
+				}
+			});
+			   
 		   	return res.render('comments', {
-          type: req.query.c,
-		   		comments: commentToSend,
-          user: req.session.user,
+				type: req.query.c,
+				comments: commentToSend,
+				user: req.session.user,
 		   	});
   		});
   	});
--- CryptoClub/controllers/usersListController.js
+++ CryptoClub/controllers/usersListController.js
@@ -1,71 +1,57 @@
+const {User, Wallet} = require('../models');
+
 module.exports = {
   getAllusers(req, res) {
-    return res.render('usersList', { user: req.session.user });
+
+    User.findAll().then(users => {
+      //* What I want to happen here is that I retreive:
+      //*   - All Users
+      //*   - All their corresponding Wallet values
+      //*   - Their profile picture URL (Not currently in the database
+
+      //* A list of keys
+      var userIds = [];
+
+      //* A map for O(1) retrieval of a User's name since wallets only carry the id of a user
+      var idMap = {};
+
+      users.forEach(user => {
+
+        userIds.push(user.id);
+
+        idMap[user.id] = user.name;
+
+      });
+      Wallet.findAll({
+        where: {
+          userId: userIds,
+        }
+      }).then(wallets => {
+
+        console.log(wallets);
+        
+        //* holds Wallet objects with:
+        //*   - Username
+        //*   - Bitcoin amount that the user has
+        //*   - Ethereum amount that the user has
+        //*   - LiteCoin amount that the user has
+        var walletToSend = [];
+        
+        wallets.forEach(wallet => {
+          console.log(wallet.id);
+
+          //* Create wallet object 
+          //TODO I stopped engineering here at 10:45pm at the CS lab @ UTRGV
+          // var walletObj = {
+          //   username = idMap[wallet.userId],
+          //   bitcoin = 
+          //     }
+          //   }),
+          
+        });
+      });
+
+    });
   },
+    return res.render('usersList', { user: req.session.user });
 };
-
-
-// const { User, Wallet, Adress, Comment } = require('../models');
-// module.exports = {
-//   getAllUsers(req, res) {
-//   	//edit this to include only from a btc type
-//   	//specifically req.body.crypto (or req.query.crypto) should have
-//   	//string of crypto that you want to see comments of
-//   	User.findAll({
-//   		where: {
-//   			expert: 1,
-//   		},
-//   	}).then(users => {
-//   		//I know I'm storing it twice Tomai don't take away points :(
-//   		//userIds is just a list of id keys for the DB to pully only comments
-//   		//from those users
-//  		// idMap is just a map for instant access for a User's name since Comments
-//  		// only carry the id of the user who made them
-//   		var userIds = [];
-//   		var idMap = {};
-//   		users.forEach(user => {
-//   			userIds.push(user.id);
-//   			idMap[user.id] = user.name;
-//   		});
-//   		Comment.findAll({
-//   			where: {
-//   				userId: userIds,
-//   			},
-//   			order: [
-//   				//sort fetched list of entries by timestamp in descending order "DESC"
-//   				['createdAt', 'DESC'],
-//   			],
-//   		}).then(comments => {
-//   			//console.log(userIds);
-//   			//console.log(comments);
-//   			// commentToSend will carry list of Comment objects with the
-//   			// following attributes: name, text, prediction, and time (time is not workign atm)
-// 		  	var commentToSend = [];
-// 		  	//need this or else all comments get displayed 
-// 		   	var commentLimit = 4;
-// 		   	//for each comment in fetched comments, we push to commentToSend
-// 		   	//(TODO:Alvaro) Make sure only one comment for each user is fetched
-// 		   	//as-is an expert can spam and take the comment space from other experts
-// 		   	comments.forEach(comment => {
-// 		   		console.log(comment.createdAt);
-// 		   		var comm = {
-// 		   			name: idMap[comment.userId],
-// 		   			text: comment.text,
-// 		   			prediction: comment.prediction,
-// 		   			//CURRENTLY CANNOT ACCESS CREATEDAT ATTRIBUTE
-// 		   			//MAY NEED TO REBUILD DATABASE
-// 		   			//HATE TIMESTAMPS REEEE
-// 		   			time: comment.createdAt,
-// 		   		}
-// 		   		if (commentLimit > 0){
-// 		   			commentToSend.push(comm);
-// 		  	 			commentLimit--;
-// 		   		}
-// 		   	});
-// 		   	return res.render('comments', {
-// 		   		comments: commentToSend,
-// 		   	});
-//   		});
-//   	});
-//   },
-// };
