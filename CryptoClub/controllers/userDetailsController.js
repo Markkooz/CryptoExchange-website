c5a1f2390ef6da4f869bdea42696db235a4ff465
Ricardo Carrillo <ricardo@doesnotexist.utpa.edu>
2019-12-06 21:28:37 -0600

    FINALLY finished the userDetails and userList modules. WE ARE DONZO

--- CryptoClub/controllers/userDetailsController.js
+++ CryptoClub/controllers/userDetailsController.js
@@ -12,50 +12,15 @@ module.exports = {
       where: {
         id: req.query.id
       },
+      include: [
+        {model: Wallet, as: 'Wallets'},
+        {model: Comment, as: 'Comments'}
+      ] 
     }).then(user => {
-
-      //* List of JSON comment objects
-      var commentsToSend = [];
-
-      //* JSON object with relevant data from userList (passed via query string), 
-      //* and a list of JSON comment objects in no particular order
-      var profile = {
-        name: user.name,
-        id: req.query.id,
-        btc: req.query.btc,
-        eth: req.query.eth,
-        ltc: req.query.ltc,
-        comments: commentsToSend
-      };
-
-      //* Pull the relevant comments from the database if user is an expert
-      if(user.expert) {
-        Comment.findAll({
-          where: {
-            userId: user.id
-          }
-        }).then(comments => {
-
-          //* Iterate through the comments
-          comments.forEach(comment => {
-
-            //* JSON object that holds a comment's text, the crypto prediction, and the crypto they are predicting
-            var comment = {
-              crypto: comment.crypto,
-              text: comment.text,
-              prediction: comment.prediction
-            };
-
-            //* Push JSON comment object into comment list
-            commentsToSend.push(comment);
-          })
-
-          //* Return the profile
-          return res.render('user', {
-            profile: profile
-          })
-        });
-      }
+      //* Return the profile
+      return res.render('user', {
+        user: user
+      });
     });
   },
 }; 
