dd220e54c540185508f3ae79b0242747a1e0133a
Ricardo Carrillo <ricardo@doesnotexist.utpa.edu>
2019-12-06 19:46:04 -0600

    Created the controller for the user details view, and edited the user.hbs to use Express Handlebars HTML generation

--- CryptoClub/controllers/userDetailsController.js
+++ CryptoClub/controllers/userDetailsController.js
@@ -1,8 +1,61 @@
 
+const {User, Wallet, Comment} = require('../models');
+
 module.exports = {
   getUser(req, res) {
-  	if (!req.session.user)
-  		return res.redirect('/login');
-    return res.render('user', {user: req.session.user });
+
+    //* Validate if they are a user or a guest
+  	if (!req.session.user) return res.redirect('/login');
+    
+    //* Pull the correct user from the database
+    User.findOne({
+      where: {
+        userId: req.query.id
+      },
+    }).then(user => {
+
+      //* Pull the relevant comments from the database if user is an expert
+      if(user.expert) {
+        Comment.findAll({
+          where: {
+            userId: user.id
+          }
+        }).then(comments => {
+
+          //* Iterate through the comments
+          comments.forEach(comment => {
+
+            //* JSON object that holds a comment's text, the crypto prediction, and the crypto they are predicting
+            var comment = {
+              crypto: comment.crypto,
+              text: comment.text,
+              prediction: comment.prediction
+            };
+
+            //* List of JSON comment objects
+            var commentsToSend = [];
+
+            //* Push JSON comment object into comment list
+            commentsToSend.push(comment);
+
+            //* JSON object with relevant data from userList (passed via query string), 
+            //* and a list of JSON comment objects in no particular order
+            var profile = {
+              name: user.name,
+              id: req.query.id,
+              btc: req.query.btc,
+              eth: req.query.eth,
+              ltc: req.query.ltc,
+              comments: commentsToSend
+            };
+          })
+
+          //* Return the profile
+          return res.render('user', {
+            profile: profile
+          })
+        });
+      }
+    });
   },
 };
